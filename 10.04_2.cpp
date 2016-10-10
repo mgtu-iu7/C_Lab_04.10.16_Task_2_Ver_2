a9bb0d3c0059b8b7364e812bf2c0fc7753c56920
loktev08 <dmitry_loktev@mail.ru>
2016-10-10 11:00:46 +0300

    Лабы C 10.04 Задача 2

---
+++ 10.04_2.cpp
@@ -0,0 +1,37 @@
+//Palindrome sequence task.
+
+#include "stdafx.h" 
+#include <conio.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a, b, inversion, base, residual;
+
+	printf("Enter positive a:\n");
+	scanf_s("%d", &a);
+
+	printf("\nEnter positive b:\n");
+	scanf_s("%d", &b);
+
+	if (a > 9 && b > a)	{
+		while (a <= b) {
+			inversion = 0;
+			base = a;
+			while (base != 0) {
+				residual = base % 10;
+				inversion = (inversion * 10) + residual;
+				base /= 10;
+			}
+			if (a == inversion) {
+				printf("\nPalindrome: %d", a);
+			}
+			a++;			
+		}
+	}
+	else {
+		printf("\nEnter a > 9 and b > a");
+	}
+
+	_getch();
+	return 0;
+}
